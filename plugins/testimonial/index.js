caaf6d1924f07c76f4d7013bf01efff0299556d0
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2021-09-21 20:57:09 +0100

    modified scroll bar

--- plugins/testimonial/index.js
+++ plugins/testimonial/index.js
@@ -20,8 +20,8 @@ $(document).ready(function () {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
