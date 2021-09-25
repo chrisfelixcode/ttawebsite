faa616ba1d64445915eb0691f8e3b0fdb46e3b1c
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2021-09-25 16:42:05 +0100

    modified scroll bar

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 
 
-var primary_btns = document.getElementsByClassName('primary-btn');
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 10) {
+        $('#main-nav').addClass('floatingNav');
+    } else {
+        $('#main-nav').removeClass('floatingNav');
+    }
+});
 
-console.log(primary_btns)
+
+var primary_btns = document.getElementsByClassName('primary-btn');
 
 for (var i = 0; i < primary_btns.length; i++) {
 
@@ -17,8 +24,7 @@ for (var i = 0; i < primary_btns.length; i++) {
             <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                 viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
                 <defs>
-                    <path id="gentle-wave"
-                        d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
                 </defs>
                 <g class="parallax">
                     <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(229,190,87,0.7" />
